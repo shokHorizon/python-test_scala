36852542a47e8660609ff3e3cc7414bb712cbb10
kostashok <53877057+shokHorizon@users.noreply.github.com>
2022-09-15 21:14:44 +0300

    Solution is ready

---
+++ main.py
@@ -0,0 +1,31 @@
+class ArrayConverter:
+    def __init__(self, array: list) -> None:
+        self.array =  self.normalize_array(array)
+
+    def normalize_array(self, array: list) -> list:
+        normalized_array = list()
+        for elem in array:
+            match elem:
+                case int():
+                    if elem < 38:
+                        normalized_array.append(str(elem))
+                case str():
+                    if elem.isnumeric() and int(elem) < 38:
+                        normalized_array.append(elem)
+                case list():
+                    for nested_elem in elem:
+                        if nested_elem < 38:
+                            normalized_array.append(str(nested_elem))
+        return normalized_array
+
+    def print_array(self):
+        print(', '.join(self.array))
+
+
+def main():
+    a = (1, 2, 3, 45, "7", [12, 45, 1], 23, "56")
+    ArrayConverter(a).print_array()
+
+
+if __name__ == '__main__':
+    main()
